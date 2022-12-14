7a90d07c0fc8c49eea0bc8d9952a87a0f5814ecf
Richard Padre <richardmpadre@gmail.com>
2022-12-13 21:43:24 -0500

    test

---
+++ BethanyPieShopHRSM/BethanyPieShopHRSM/Components/EmployeeCard.razor.cs
@@ -0,0 +1,11 @@
+﻿using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanyPieShopHRSM.Components
+{
+    public partial class EmployeeCard
+    {
+        [Parameter]
+        public Employee Employee { get; set; } = default!;
+    }
+}
